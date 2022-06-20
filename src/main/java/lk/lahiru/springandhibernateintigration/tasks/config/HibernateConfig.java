9722430000879e66b6455b08f5352fc2f8ae2db5
LahiruRJ <sanlahiru720@gmail.com>
2022-06-20 12:08:13 +0530

    Add HibernateConfig class

--- src/main/java/lk/lahiru/springandhibernateintigration/tasks/config/HibernateConfig.java
+++ src/main/java/lk/lahiru/springandhibernateintigration/tasks/config/HibernateConfig.java
@@ -1,5 +1,29 @@
 package lk.lahiru.springandhibernateintigration.tasks.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+
+
 public class HibernateConfig {
 
+    @Bean
+    public DataSource dataSource(){
+        JndiObjectFactoryBean jndiDataSource = new JndiObjectFactoryBean();
+        jndiDataSource.setJndiName("java:comp/env/jdbc/pool");
+        return (DataSource) jndiDataSource.getObject();
+    }
+
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource){
+        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+        lsfb.setDataSource(dataSource);
+        lsfb.setPackagesToScan("lk.ijse.dep8.tasks.entity");
+        return lsfb;
+    }
 }
